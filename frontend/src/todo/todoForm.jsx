10d2425ba77eefe84f7fc286bde6c2102e03cb1f
Caio Willian <caiow.willian@gmail.com>
2017-08-06 02:47:02 -0300

    Integração com mongodb não funcionando

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,7 +5,7 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div className='todoForm' role='form'>
         <Grid cols='12 9 11'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa' values={props.description} onChange={props.handleChange}></input>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus' onClick={props.handleAdd}/> 
